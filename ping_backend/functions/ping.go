39c9365ac20e8651db937f6f68272d98896f0c6c
taubyte <samy@taubyte.com>
2022-09-21 12:58:31 -0500

    Added ping example

---
+++ ping_backend/functions/ping.go
@@ -0,0 +1,18 @@
+package lib
+
+import (
+    "bitbucket.org/taubyte/go-sdk/event"
+)
+
+//export ping
+func ping(e event.Event) uint32 {
+    h, err := e.HTTP()
+	if err != nil {
+		return 1
+	}
+
+
+	h.Write([]byte("PONG"))
+
+    return 0
+}
