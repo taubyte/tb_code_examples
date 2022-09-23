8643e7f81b951e3fb2b855aeb0edb2f9241a29ae
taubyte <samy@taubyte.com>
2022-09-22 23:01:23 -0500

    Added avatar generation example

---
+++ avatar_backend/functions/goavatar.go
@@ -0,0 +1,55 @@
+package lib
+
+import (
+	"bitbucket.org/taubyte/go-sdk/event"
+)
+
+//export avatar
+func avatar(e event.Event) uint32 {
+	h, err := e.HTTP()
+	if err != nil {
+		return 1
+	}
+
+	gender, _ := h.Query().Get("gender")
+	if gender == "" {
+		if time.Now().UnixNano()%2 == 0 {
+			gender = "female"
+		} else {
+			gender = "male"
+		}
+	}
+
+	username, _ := h.Query().Get("username")
+	if username == "" {
+		username = time.Now().String()
+	}
+
+	var _gender govatar.Gender
+	switch strings.ToLower(gender) {
+	case "male", "m":
+		_gender = govatar.MALE
+	case "female", "f":
+		_gender = govatar.FEMALE
+	}
+
+	var b bytes.Buffer
+
+	img, err := govatar.GenerateForUsername(_gender, username)
+	if err != nil {
+		h.Write([]byte(err.Error()))
+		return 1
+	}
+
+	err = png.Encode(&b, img)
+	if err != nil {
+		h.Write([]byte(err.Error()))
+		return 1
+	}
+
+	h.Headers().Set("Content-Type", "image/png")
+	h.Write(b.Bytes())
+
+	return 0
+}
+
